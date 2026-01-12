36717b716ebee561d076cbff088f75cd853fa4d6
Chinnasamy7 <chinnasamymurugan2005@gmail.com>
2026-01-12 10:59:43 +0530

    Enhanced login feature in separate branch

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -3,10 +3,10 @@ import React from "react";
 function Login() {
   return (
     <div>
-      <h2>User Login</h2>
+      <h2>Login Feature</h2>
       <input placeholder="Email" />
       <input placeholder="Password" type="password" />
-      <button>Login</button>
+      <button>Sign In</button>
     </div>
   );
 }
